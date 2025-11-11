a49602055e22b964817ed882b4003f19727bedc9
Francisco Lino <juanfranlino321@gmail.com>
2025-11-11 20:47:29 -0300

    Merge main into Rama_Francisco
    # Conflicts:
    #	Obligatorio-1-P2/src/MedioTateti/Jugada.java
    #	Obligatorio-1-P2/src/MedioTateti/Pieza.java

--- src/Recursos/VentanaAutores.java
+++ src/Recursos/VentanaAutores.java
@@ -59,27 +59,7 @@ public class VentanaAutores extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
    
-    public static void main(String args[]) {
-       
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ReflectiveOperationException | javax.swing.UnsupportedLookAndFeelException ex) {
-            logger.log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-       
-        java.awt.EventQueue.invokeLater(() -> new VentanaAutores().setVisible(true));
-    }
+    
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;
